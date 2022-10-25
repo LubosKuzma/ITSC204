e15b7b146ae3a867691658b8f4a6e5becbb4fc14
Gurkirat353 <116184980+Gurkirat353@users.noreply.github.com>
2022-10-25 00:35:19 -0400

    x64_toolchain.sh
    64bit default for the toolchain

--- scripts/x86_toolchain.sh
+++ scripts/x86_toolchain.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
 
 # Created by Lubos Kuzma
 # ISS Program, SADT, SAIT
@@ -8,14 +7,14 @@
 if [ $# -lt 1 ]; then
 	echo "Usage:"
 	echo ""
-	echo "x86_toolchain.sh [ options ] <assembly filename> [-o | --output <output filename>]"
+	echo "x64_toolchain.sh [ options ] <assembly filename> [-o | --output <output filename>]"
 	echo ""
 	echo "-v | --verbose                Show some information about steps performed."
 	echo "-g | --gdb                    Run gdb command on executable."
 	echo "-b | --break <break point>    Add breakpoint after running gdb. Default is _start."
 	echo "-r | --run                    Run program in gdb automatically. Same as run command inside gdb env."
 	echo "-q | --qemu                   Run executable in QEMU emulator. This will execute the program."
-	echo "-64| --x86-64                 Compile for 64bit (x86-64) system."
+	echo "-32| --x86                	Compile for 32bit (x86) system."
 	echo "-o | --output <filename>      Output filename."
 
 	exit 1
@@ -25,7 +24,7 @@ POSITIONAL_ARGS=()
 GDB=False
 OUTPUT_FILE=""
 VERBOSE=False
-BITS=False
+BITS=True   # Bits true default
 QEMU=False
 BREAK="_start"
 RUN=False
@@ -44,8 +43,8 @@ while [[ $# -gt 0 ]]; do
 			VERBOSE=True
 			shift # past argument
 			;;
-		-64|--x84-64)
-			BITS=True
+		-32|--x86)
+			BITS=False
 			shift # past argument
 			;;
 		-q|--qemu)
